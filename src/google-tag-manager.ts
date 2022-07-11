62f7dbebcc679383722dbe47bc58506f53507f90
JonathanTGold <jonathan.gold@86@gmail.com>
2022-07-11 11:12:08 +0200

    feat(FEC-12380): Add configuration option to listen to all events

--- src/google-tag-manager.ts
+++ src/google-tag-manager.ts
@@ -1,16 +1,41 @@
-import { BasePlugin, FakeEvent, KalturaPlayer } from 'kaltura-player-js';
+import { BasePlugin, EventTypes, FakeEvent, KalturaPlayer } from 'kaltura-player-js';
 import { HEAD_TAG, BODY_TAG } from './gtm-tags';
 
 export const pluginName = 'googleTagManager';
 
+export type PresetOption = keyof GoogleTagManagerConfig['customEventsTracking']['preset'];
+
 export interface GoogleTagManagerConfig {
   containerId: string;
-  customEvents: string[];
+  customEventsTracking: {
+    custom: string[];
+    preset: {
+      coreEvents: boolean;
+      UIEvents: boolean;
+      playlistEvents: boolean;
+      castEvents: boolean;
+    };
+  };
 }
+
+export const eventTypesMap: { [presetOption: string]: keyof EventTypes } = {
+  castEvents: 'Cast',
+  coreEvents: 'Core',
+  playlistEvents: 'Playlist',
+  UIEvents: 'UI'
+};
 
 export class GoogleTagManager extends BasePlugin<GoogleTagManagerConfig> {
   protected static defaultConfig = {
-    customEvents: []
+    customEventsTracking: {
+      custom: [],
+      preset: {
+        coreEvents: false,
+        UIEvents: false,
+        playlistEvents: false,
+        castEvents: false
+      }
+    }
   };
 
   constructor(name: string, player: KalturaPlayer, config: GoogleTagManagerConfig) {
@@ -48,11 +73,35 @@ export class GoogleTagManager extends BasePlugin<GoogleTagManagerConfig> {
   }
 
   private initCustomEventsListeners(): void {
-    this.config.customEvents.forEach((customEvent) => {
-      this.eventManager.listen(this.player, customEvent, (event: FakeEvent) => {
-        const dataLayerVariablePayload = event.payload !== undefined ? { [`${event.type}-payload`]: event.payload } : {};
-        window.dataLayer.push({ event: event.type, ...dataLayerVariablePayload });
-      });
+    this.config.customEventsTracking.custom.forEach((customEvent) => {
+      this.trackCustomEvent(customEvent);
+    });
+
+    for (const [presetOption, isTrackEnabled] of Object.entries<boolean>(this.config.customEventsTracking.preset)) {
+      if (isTrackEnabled) {
+        const eventType = eventTypesMap[presetOption];
+        Object.values(this.player.Event[eventType]).forEach((customEvent) => {
+          if (!this.config.customEventsTracking.custom.includes(customEvent)) {
+            if (
+              ![
+                this.player.Event.Core.TIME_UPDATE,
+                this.player.Event.Core.PROGRESS,
+                this.player.Event.Core.FPS_DROP,
+                this.player.Event.Core.FRAG_LOADED
+              ].includes(customEvent)
+            ) {
+              this.trackCustomEvent(customEvent);
+            }
+          }
+        });
+      }
+    }
+  }
+
+  private trackCustomEvent(customEvent: string): void {
+    this.eventManager.listen(this.player, customEvent, (event: FakeEvent) => {
+      const dataLayerVariablePayload = event.payload !== undefined ? { [`${event.type}-payload`]: event.payload } : {};
+      window.dataLayer.push({ event: event.type, ...dataLayerVariablePayload });
     });
   }
 
